eddf765f3f78cb3c4bebaaeb7098054a1ee13d87
Sandip_Dutta_8 <ayandutta2005@gmail.com>
2024-08-24 20:33:05 +0530

    note card ui added

---
+++ frontend/src/components/Notecard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { MdOutlinePushPin, MdCreate, MdDelete } from 'react-icons/md'
+
+const Notecard = ({ title, date, content, tags, isPinned, onDelete, onPinNote, onEdit }) => {
+    return (
+        <div className='border rounded-md p-4 bg-white drop-shadow-sm hover:drop-shadow-md transition-all ease-in-out'>
+            <div className='flex items-center justify-between'>
+                <div>
+                    <h6 className='text-md font-medium'>{title}</h6>
+                    <span className='text-xs text-slate-500'>{date}</span>
+                </div>
+
+                <div className='w-10 h-10 flex items-center justify-center rounded-full hover:bg-slate-200'>
+                    <MdOutlinePushPin className={`icon-btn ${isPinned ? "text-primary" : "text-slate-300"} `} onClick={onPinNote} />
+                </div>
+            </div>
+
+            <p className='text-sm text-slate-500 mt-2'>{content?.slice(0, 60)}</p>
+
+            <div className='flex items-center justify-between mt-2'>
+                <div className='text-sm text-slate-500'>{tags}</div>
+
+                <div className='flex items-center gap-2'>
+                    <MdCreate className='icon-btn hover:text-green-600 ' onClick={onEdit} />
+                    <MdDelete className='icon-btn hover:text-red-600 ' onClick={onDelete} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Notecard
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
+import Notecard from '../components/Notecard'
+import { MdAdd } from 'react-icons/md'
 
 const Home = () => {
   return (
     <>
       <Navbar />
+
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-3 gap-4 mt-10 mx-10'>
+          <Notecard
+            title={"Do coding"}
+            date={"24th aug"}
+            tags={"#coding"}
+            content={"Do coding daily atleast 1hr. Web dev and solve DSA questions from the stiver sheet"}
+            isPinned={true}
+            onDelete={() => { }}
+            onEdit={() => { }}
+            onPinNote={() => { }}
+          />
+          <Notecard
+            title={"Do coding"}
+            date={"24th aug"}
+            tags={"#coding"}
+            content={"Do coding daily atleast 1hr. Web dev and solve DSA questions from the stiver sheet"}
+            isPinned={true}
+            onDelete={() => { }}
+            onEdit={() => { }}
+            onPinNote={() => { }}
+          />
+        </div>
+
+        <button className='w-16 h-16 flex items-center justify-center bg-primary rounded-full hover:bg-blue-600 fixed right-10 bottom-10 z-10' onClick={() => {}}>
+          <MdAdd className='text-[32px] text-slate-100'/>
+        </button>
+      </div>
     </>
   )
 }
